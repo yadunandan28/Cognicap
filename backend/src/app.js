5ef943f391e1285b781acdc8bbb023bb6d0a985c
yadunandan28 <yadunandan28704@gmail.com>
2026-02-22 12:14:36 +0530

    ..

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,22 @@
 const express = require("express");
-const cors = require("cors");
-const helmet = require("helmet");
-const morgan = require("morgan");
+const cors    = require("cors");
+const helmet  = require("helmet");
+const morgan  = require("morgan");
 require("dotenv").config();
 
 const sessionRoutes = require("./routes/session.routes");
+const captchaRoutes = require("./routes/captcha.routes");
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*"   // allow all origins in production for now
+}));
 app.use(helmet());
 app.use(morgan("dev"));
 
 app.use("/api/session", sessionRoutes);
+app.use("/api/captcha", captchaRoutes);
 
 module.exports = app;
